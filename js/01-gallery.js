88a451834f6456a75e9c7e9847722e7381c515e1
NazarkoKril <122899742+NazarkoKril@users.noreply.github.com>
2023-06-30 19:16:02 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+createGallery();
+
+function createGallery() {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
+
+galleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const onImageShow = basicLightbox.create(`<img src="${e.target.dataset.source}">`, {
+    onShow() {
+      window.addEventListener('keydown', onEsc);
+    },
+
+    onClose() {
+      window.removeEventListener('keydown', onEsc);
+    },
+  });
+
+  onImageShow.show();
+
+
+  function onEsc(e) {
+    if (e.code !== 'Escape') {
+      return;
+    }
+    onImageShow.close();
+  }
+}
+
+console.log(galleryItems)
